d5a625437127a5bc749308e317530050ff658d5f
Lenny Duan <dhy939694@gmail.com>
2016-10-31 20:28:06 +0000

    models v1.1

--- models/order.js
+++ models/order.js
@@ -1,13 +1,7 @@
 // Dependencies
-<<<<<<< HEAD
 var mongoose = require('mongoose');
-=======
-var restful = require('node-restful');
-var mongoose = restful.mongoose;
->>>>>>> 8203f2d999066b112cd130a999330d122debc0db
 
 // Schema
-
 var orderSchema = mongoose.Schema({
 	orderId: Number,
 	orderName: String,
@@ -18,8 +12,5 @@ var orderSchema = mongoose.Schema({
 	clientId: Number,
 });
 
-<<<<<<< HEAD
 var Order = module.exports = mongoose.model('Orders', orderSchema);
-=======
-module.exports = restful.model('Orders', orderSchema);
->>>>>>> 8203f2d999066b112cd130a999330d122debc0db
+
