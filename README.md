e36dd6b63f89f0e5f5bc4fdd30c647162de21290
fang <ekinrf@gmail.com>
2016-10-31 22:45:31 +0000

    Update Readme test section.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # couponsys
 The coupon service.
+
+##Test
+
+Run test:
+'''
+gulp test
+'''
+
+Setup test watch task:
+
+'''
+gulp test-watch
+'''
--- api/v1/index.js
+++ api/v1/index.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.send(
     {
       version: "V1",
-      description: 'Daoyuan Edu Coupon Service.'
+      description: 'Daoyuan Edu Coupon Service.',
+      author: "fang"
     });
 });
 
