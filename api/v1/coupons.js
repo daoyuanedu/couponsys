6588c0c31faed17f6000a3b9902af98eb3940b4f
Mr. White <hod8@aber.ac.uk>
2016-10-31 19:47:02 +0000

    change 'router' folder to 'api/v1' (#2)
    * Test From Hduan
    
    * Add Empty Folder
    
    * modify framework api/v1
    
    * Delete useless route

--- api/v1/coupons.js
+++ api/v1/coupons.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('This will list all coupons');
 });
 
 module.exports = router;
--- api/v1/index.js
+++ api/v1/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Render Page use template in ./views/index
+  res.render('index', { title: 'Coupons' });
 });
 
 module.exports = router;
