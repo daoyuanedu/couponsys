379ddf229ee923788528cfc9df69f4e1c2939b78
fang <ekinrf@gmail.com>
2016-10-31 22:09:50 +0000

    Test dependency added. Supertest + chai . Provide a sample test for api index.

--- api/v1/index.js
+++ api/v1/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // Render Page use template in ./views/index
-  res.render('index', { title: 'Coupons' });
+router.get('/', function (req, res, next) {
+  res.send(
+    {
+      version: "V1",
+      description: 'Daoyuan Edu Coupon Service.'
+    });
 });
 
 module.exports = router;
--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,17 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-    return gulp.src(['./test/*.js'], { read: false })
-        .pipe(mocha({
-            reporter: 'spec',
-            globals: {
-                should: require('should')
-            }
-        }));
+
+gulp.task('test', function () {
+  gulp.src(['./test/**/*.test.js'], {read: false}).pipe(mocha({
+    reporter: 'list',
+    globals: {
+      should: require('chai').should(),
+      expect: require('chai').expect(),
+    }
+  })).on('error', gutil.log);
 });
 
-gulp.task('test', function() {
-    gulp.
-    src(['./test/*.js'], {read: false}).
-    pipe(mocha({ reporter: 'list' }))
-        .on('error', gutil.log);
-});
-
-gulp.task('watch', function() {
-    gulp.watch('./*.js', ['test']);
+gulp.task('watch', function () {
+  gulp.watch('./*.js', ['test']);
 });
