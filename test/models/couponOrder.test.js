45d875edc92b489d30d9529a4e78d58eed838a1b
Lenny Duan <dhy939694@gmail.com>
2016-11-02 23:40:22 +0000

    models test

--- test/models/couponOrder.test.js
+++ test/models/couponOrder.test.js
@@ -15,6 +15,20 @@ describe("CouponOrder Model", function () {
     rebateValue: 0
   };
 
+  var couponOrderWithSameOrderID = {
+    orderID : "normal1order",
+    couponID: "coupon2normal",
+    rebated: false,
+    rebateValue: 0
+  };
+
+  var couponOrderWithSameCouponID = {
+    orderID : "normal2order",
+    couponID: "coupon1normal",
+    rebated: false,
+    rebateValue: 0
+  };
+
   var couponOrderWithoutOrderID = {
     orderID : null,
     couponID: "coupon1normal",
@@ -26,6 +40,13 @@ describe("CouponOrder Model", function () {
     orderID : "normal1order",
     couponID: null,
     rebated: false,
+    rebateValue: 0
+  };
+
+  var couponOrderWithoutRebated = {
+    orderID : "normal1order",
+    couponID: "coupon1normal",
+    rebated: null,
     rebateValue: 0
   };
 
@@ -41,7 +62,7 @@ describe("CouponOrder Model", function () {
     couponOrder.save(done);
   });
 
-  it("should not be able to save a non-orderId couponOrder to the db", function(err)) {
+  it("should not be able to save a non-orderId couponOrder to the db", function(done)) {
     var couponOrder = new CouponOrder(couponOrderWithoutOrderID);
     couponOrder.save(function (err) {
       if(err) done();
@@ -51,7 +72,7 @@ describe("CouponOrder Model", function () {
     });
   });
 
-  it("should not be able to save a non-couponID couponOrder to the db", function(err)) {
+  it("should not be able to save a non-couponID couponOrder to the db", function(done)) {
     var couponOrder = new CouponOrder(couponOrderWithoutCouponID);
     couponOrder.save(function (err) {
       if(err) done();
@@ -61,5 +82,63 @@ describe("CouponOrder Model", function () {
     });
   });
 
+ it("should not be able to save a non-rebated couponOrder to the db", function(done)) {
+    var couponOrder = new CouponOrder(couponOrderWithoutRebated);
+    couponOrder.save(function (err) {
+      if(err) done();
+      else{
+        throw done(err);
+      }
+    });
+  });
+
+  it("should read an exiting couponOrder from the db", function (done) {
+    var couponOrder = new CouponOrder(couponOrderNormal);
+    couponOrder.save(function (err) {
+      if(err) done(err);
+      else{
+        CouponOrder.findOne({orderID : 'normal1order'}, function (err, couponOrder) {
+          if(err) throw done(err);
+          else{
+            (couponOrder.couponID).should.equal('coupon1normal');
+            (couponOrder.couponID).should.equal(couponOrderNormal.couponID);
+            done();
+          }
+        })
+      }
+    });
+  });
+
+  it("should failed to save conponOrders with same orderID", function (done) {
+    var couponOrder = new CouponOrder(normal1order);
+    var sameCouponOrder = new CouponOrder(couponOrderWithSameOrderID);
+    CouponOrder.save(function (err) {
+      if(err) done(err);
+      else{
+        sameCouponOrder.save(function (err)) {
+          if(err) done(); 
+          else {
+            throw done(err);
+          }
+        });
+      }
+    }
+  });
+
+  it("should be able to save conponOrders with same CouponID", function (done) {
+    var couponOrder = new CouponOrder(normal1order);
+    var sameCouponOrder = new CouponOrder(couponOrderWithSameCouponID);
+    CouponOrder.save(function (err) {
+      if(err) done(err);
+      else{
+        sameCouponOrder.save(function (err)) {
+          if(err) done(err); 
+          else {
+            done();
+          }
+        });
+      }
+    }
+  });
 
 });
