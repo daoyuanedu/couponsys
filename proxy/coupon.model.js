04130e5da81d7273c376fee1b8bf3daa043490a3
ekinrf <ekinrf@users.noreply.github.com>
2016-11-06 16:31:34 +0000

    Model v2 (#27)
    * Update coupon model to include valid property.
    Update api raml for some examples.
    
    * Added CouponOrder proxy and some proxy tests.

--- proxy/coupon.model.js
+++ proxy/coupon.model.js
@@ -7,5 +7,14 @@ var Coupon = require('../models').Coupon;
 
 exports.getCouponsByUsername = function (username) {
   //return a Promise
-  return Coupon.find({ username : username});
+  return Coupon.find({ username : username}, {_id : 0, __v : 0});
+};
+
+exports.isCouponValid = function (couponId) {
+  return Coupon.find({ couponID : couponId}).then(function (coupons) {
+    if(coupons.length === 0) return false;
+    else return coupons[0].valid;
+  }, function (err) {
+    return err;
+  });
 };
