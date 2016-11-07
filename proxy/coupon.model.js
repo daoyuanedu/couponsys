e4cd2be53cd9a1c074fa6a43dc08a5da07781640
Lenny Duan <dhy939694@gmail.com>
2016-11-07 22:07:21 +0000

    typo error

--- proxy/coupon.model.js
+++ proxy/coupon.model.js
@@ -5,7 +5,7 @@
  */
 var Coupon = require('../models').Coupon;
 
-exports.getCouponsAll = function function_name(argument) {
+exports.getAllCoupons = function function_name(argument) {
 	return Coupon.find();
 };
 
