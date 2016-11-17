c2f661847d7076117b10dcdaad0d05b6d8a9f921
Hongyi Duan <hongyi_duan@mentor.com>
2016-11-17 10:09:35 +0000

    Fix PR

--- controller/v1/coupons.js
+++ controller/v1/coupons.js
@@ -1,5 +1,25 @@
+var couponProxy = require('../../proxy/coupon.model');
+var Coupon = require('../../models').Coupon;
+
+// For UI test, need to delete later
+var couponData = require('../../test/common/modelCouponTestData');
+var saveToUITest = function () {
+   new Coupon(couponData.userACouponPerc1).save();
+   new Coupon(couponData.userACouponCash1).save();
+   new Coupon(couponData.user1Coupon).save();
+   new Coupon(couponData.userAWithPercRule).save();
+   new Coupon(couponData.userBWithCashRule).save();
+   new Coupon(couponData.userBWithInvalidCoupon).save();
+};
+
 // Coupons
 var coupons = function (req, res) {
- res.render('pages/coupons');
+	saveToUITest();
+  couponProxy.getAllCoupons().then(function (coupons) {
+  	res.render('pages/coupons', 
+  		{
+  			CouponList: coupons
+  		});
+  });
 };
 exports.coupons = coupons;
--- controller/v1/orders.js
+++ controller/v1/orders.js
@@ -1,5 +1,23 @@
+var couponOrderProxy = require('../../proxy/couponOrder.model');
+var couponOrder = require('../../models').CouponOrder;
+
+// For UI test, need to delete later
+var couponOrderData = require('../../test/common/modelCouponOrderTestData');
+var saveToUITest = function () {
+   new couponOrder(couponOrderData.orderUsingUserACouponCash1Rebated).save();
+   new couponOrder(couponOrderData.orderUsingUserACouponCash1NotRebated).save();
+   new couponOrder(couponOrderData.couponOrderNormal).save();
+   new couponOrder(couponOrderData.couponOrderWithSameCouponID).save();
+};
+
 // Orders
 var orders = function (req, res) {
- res.render('pages/orders');
+  saveToUITest();
+  couponOrderProxy.getAllOrders().then(function (orders) {
+    res.render('pages/orders', 
+      {
+        OrderList: orders
+      });
+  });
 };
 exports.orders = orders;
