3c72a357f6913a9dd196446eab62db1d4b37cc64
Hongyi Duan <hongyi_duan@mentor.com>
2016-11-17 13:43:00 +0000

    event for seachbar

---
+++ public/v1/daoyuan/js/components.js
@@ -0,0 +1,10 @@
+// ------------- Function JS -----------------
+
+// Click Search button to search special CouponCode
+$(document).ready(function(){
+    $("#couponSearch").click(function(){
+        alert("You Search: " + $("#couponSearchInput").val() + " Loading...");
+    });
+});
+
+// ------------- UI JS -----------------
