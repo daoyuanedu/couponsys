7e0b93ecf22517d047c17154e07780e210def256
Lenny Duan <dhy939694@gmail.com>
2016-11-23 23:04:19 +0000

    half update

--- public/v1/daoyuan/js/couponEdit.js
+++ public/v1/daoyuan/js/couponEdit.js
@@ -15,7 +15,58 @@ $(document).ready(function(){
         xhr.setRequestHeader('x-access-token', getCookieByName('x-access-token'));
       },
       success: function(data, textStatus, xhr)
+      { 
+        alert("Have delete coupen: " + couponID);
+        console.log(xhr.status + getCookieByName('x-access-token'));
+        location.reload();
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        if(XMLHttpRequest.status === 403) {
+          alert("Please Login");
+          //location.replace('/views/login/');
+        }
+        console.log(XMLHttpRequest.status);
+      }
+    });
+    event.preventDefault();
+  });
+});
+
+// Click Button to delete Coupon
+
+$(document).ready(function(){
+  $('#updateCouponForm').submit(function( event ) {
+    var couponID = $("#update-couponCode").val();
+    var putUrl = "../../api/v1/coupons/" + couponID;
+
+    var couponRule = {
+      type: $('#update-coupon-rule-type').val(),
+      value: $('#update-coupon-rule-value').val()
+    };
+    var rebateRule = {
+      type: $('#update-rebate-rule-type').val(),
+      value: $('#update-rebate-rule-value').val()
+    };
+
+    var data = {
+     username: $('#update-username').val(),
+     couponRule: couponRule,
+     rebateRule: rebateRule,
+     valid: $('#update-valid').val()
+    };
+    
+    console.log(data);
+    $.ajax({
+      type: "PUT",
+      url: putUrl,
+      data: data,
+      beforeSend: function(xhr)
       {
+        xhr.setRequestHeader('x-access-token', getCookieByName('x-access-token'));
+      },
+      success: function(data, textStatus, xhr)
+      { 
+        alert("Have updated coupen: " + couponID);
         console.log(xhr.status + getCookieByName('x-access-token'));
         location.reload();
       },
