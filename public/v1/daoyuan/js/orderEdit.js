412a8de7a6d4fcf7de748e66460886f80c68a75f
Lenny Duan <dhy939694@gmail.com>
2016-12-03 22:58:27 +0000

    auto indent

--- public/v1/daoyuan/js/orderEdit.js
+++ public/v1/daoyuan/js/orderEdit.js
@@ -1,75 +1,75 @@
 var addCheckBox = function() {
 
-$(function () {
+  $(function () {
     $('.list-group.checked-list-box .orderList-element').each(function () {
-        
-        // Settings
-        var $widget = $(this),
-            $checkbox = $('<input type="checkbox" class="hidden" />'),
-            settings = {
-                on: {
-                    icon: 'glyphicon glyphicon-check'
-                },
-                off: {
-                    icon: 'glyphicon glyphicon-unchecked'
-                }
-            };
-        
-        $widget.css('cursor', 'pointer')
-        $widget.append($checkbox);
 
-        // Event Handlers
-        $widget.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-            $checkbox.triggerHandler('change');
-            updateDisplay();
-        });
-        $checkbox.on('change', function () {
-            updateDisplay();
-        });
-
-        // Actions
-        function updateDisplay() {
-            var isChecked = $checkbox.is(':checked');
-
-            // Set the button's state
-            $widget.data('state', (isChecked) ? "on" : "off");
-
-            // Set the button's icon
-            $widget.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$widget.data('state')].icon);
-
-            if (isChecked) {
-                $widget.addClass('forRebate');
-            } else {
-                $widget.removeClass('forRebate');
-            }
+      // Settings
+      var $widget = $(this),
+      $checkbox = $('<input type="checkbox" class="hidden" />'),
+      settings = {
+        on: {
+          icon: 'glyphicon glyphicon-check'
+        },
+        off: {
+          icon: 'glyphicon glyphicon-unchecked'
         }
+      };
 
-        // Initialization
-        function init() {
-            
-            if ($widget.data('checked') == true) {
-                $checkbox.prop('checked', !$checkbox.is(':checked'));
-            }
-            updateDisplay();
-            $widget.append('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+      $widget.css('cursor', 'pointer')
+      $widget.append($checkbox);
 
+      // Event Handlers
+      $widget.on('click', function () {
+        $checkbox.prop('checked', !$checkbox.is(':checked'));
+        $checkbox.triggerHandler('change');
+        updateDisplay();
+      });
+      $checkbox.on('change', function () {
+        updateDisplay();
+      });
+
+      // Actions
+      function updateDisplay() {
+        var isChecked = $checkbox.is(':checked');
+
+        // Set the button's state
+        $widget.data('state', (isChecked) ? "on" : "off");
+
+        // Set the button's icon
+        $widget.find('.state-icon')
+        .removeClass()
+        .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+        if (isChecked) {
+          $widget.addClass('forRebate');
+        } else {
+          $widget.removeClass('forRebate');
         }
-        init();
+      }
+
+      // Initialization
+      function init() {
+
+        if ($widget.data('checked') == true) {
+          $checkbox.prop('checked', !$checkbox.is(':checked'));
+        }
+        updateDisplay();
+        $widget.append('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+
+      }
+      init();
     });
-    
+
     $('#get-checked-data').on('click', function(event) {
-        event.preventDefault(); 
-        var rebateItems = [], counter = 0;
-        $("#orderList-ul li.forRebate").each(function(idx, li) {
-            rebateItems[counter] = creatUpdateOrder(li);
-            counter++;
-        });
-        console.log (rebateItems);
+      event.preventDefault();
+      var rebateItems = [], counter = 0;
+      $("#orderList-ul li.forRebate").each(function(idx, li) {
+        rebateItems[counter] = creatUpdateOrder(li);
+        counter++;
+      });
+      console.log (rebateItems);
     });
-});
+  });
 }
 
 var creatUpdateOrder = function(li) {
